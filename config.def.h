0adf8e62d3e9334b6ed0595722843845d569283d
210mlt <210mlt@gmail.com>
2022-10-26 05:37:39 +0200

    Added mastermaster controll defaults

--- config.def.h
+++ config.def.h
@@ -34,6 +34,8 @@ static const Rule rules[] = {
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float hfact     = 0.45; /* factor of master area hight size [0.05..0.95] */
+static const int immaster    = 2;    /* index of main master min. 1*/
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -70,8 +72,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+        { MODKEY|ShiftMask,             XK_i,      incimmaster,    {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_d,      incimmaster,    {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+        { MODKEY|ShiftMask,             XK_h,      sethfact,       {.f = -0.05} },
+        { MODKEY|ShiftMask,             XK_l,      sethfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
